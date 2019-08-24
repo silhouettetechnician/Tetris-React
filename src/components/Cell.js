dff4f0fcb47f8a618788a2b6b83b736627d39980
Timothy Millward <silhouettetechnician@gmail.com>
2019-08-24 10:37:47 +0100

    working on styling

---
+++ src/components/Cell.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyledCell } from './styles/StyledCell'
+import { TETROMINOS } from '../tetrominos'
+
+const Cell = ({ type }) => (
+    
+        <StyledCell type={'J'} color={TETROMINOS['J'].color}>Cell</StyledCell>
+     ) 
+ 
+export default Cell
+ 
+
+
---
+++ src/components/Tetris.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
+import Stage from './Stage'
+import Display from './Display'
+import StartButton from './StartButton'
+import { createStage } from '../gameHelpers'
+
+
+
+const Tetris = () => {
+    return (
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()} />
+                <aside>
+                    <div>
+                        <Display text='Score' />
+                        <Display text='Rows' />
+                        <Display text='Level' />
+                    </div>
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
+    )
+}
+
+export default Tetris
